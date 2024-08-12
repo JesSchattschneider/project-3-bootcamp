119ef3b8be5c959a865c6bedf2c053d2024b79ad
JesSchattschneider <jessica.leiria@gmail.com>
2024-08-12 12:31:24 +1200

    add: dbt models as assets

--- data-orchestration/dagster/analytics/__init__.py
+++ data-orchestration/dagster/analytics/__init__.py
@@ -1,10 +1,14 @@
 from dagster import Definitions
 from analytics.jobs import my_snowflake_job, update_wfs_job
 from analytics.resources import snowflake_resource
+from analytics.assets.dbt.dbt import dbt_warehouse, dbt_warehouse_resource
 
 defs = Definitions(
     jobs=[my_snowflake_job, update_wfs_job],
     resources={
-        "snowflake_resource": snowflake_resource
-    }
+        "snowflake_resource": snowflake_resource,
+        "dbt_warehouse_resource": dbt_warehouse_resource  # Add the dbt resource
+
+    },
+    assets=[dbt_warehouse]  # Add the dbt assets
 )
